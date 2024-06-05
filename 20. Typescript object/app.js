aaaefd160ff1718dc025f10a56de83a28b9fb4f7
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:11:27 -0700

    Add files via upload

---
+++ 20. Typescript object/app.js
@@ -0,0 +1,3 @@
+"use strict";
+let car = { make: "Toyota", model: "Corolla", year: 2022, color: "Red" };
+console.log(`Car info: ${car.make}, ${car.model}, ${car.color}, ${car.year}`);
---
+++ 20. Typescript object/app.ts
@@ -0,0 +1,4 @@
+let car: { make: string; model: string; year: number; color: string} =
+  { make: "Toyota", model: "Corolla", year: 2022, color: "Red" };
+
+console.log(`Car info: ${car.make}, ${car.model}, ${car.color}, ${car.year}`);
